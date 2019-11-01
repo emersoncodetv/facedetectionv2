f620a2d219932392b05ee849d639eb1ab304a8db
Emerson <e@serendpia.co>
2019-11-01 08:35:43 -0500

    Cambiando a navigator.mediaDevices.getUserMediapara tener soporte en todos los navegadores.

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,18 +1,23 @@
 const video = document.getElementById("video");
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-  faceapi.nets.faceExpressionNet.loadFromUri("/models")
+  faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+  faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("./models")
 ]).then(startVideo);
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => (video.srcObject = stream),
-    err => console.error(err)
-  );
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(stream => {
+      this.video.srcObject = stream;
+      /* use the stream */
+    })
+    .catch(function(err) {
+      console.error(err);
+      /* handle the error */
+    });
 }
 
 video.addEventListener("play", () => {
